ee4f3fcd59307a0f8e538fc93e7a7aafd222ab5f
Ivan Gutorov <gutorov.ia1@gmail.com>
2025-05-23 18:38:56 +0400

    add react to proj

--- UryupinskUniversity/vite.config.js
+++ UryupinskUniversity/vite.config.js
@@ -1,22 +1,16 @@
+import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    root: "src", // Указываем, что корень проекта - папка src
+    plugins: [react()],
+    root: "src",
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "src/index.html"),
-                applicants: resolve(__dirname, "src/applicants.html"),
-                contacts: resolve(__dirname, "src/contacts.html"),
-                faculties: resolve(__dirname, "src/faculties.html"),
-                research: resolve(__dirname, "src/research.html"),
             },
-            outDir: "../dist", // Папка для сборки
+            outDir: "../dist",
         },
-    },
-    server: {
-        port: 5173,
-        open: "/index.html", // Автоматически открывать index.html
     },
 });
