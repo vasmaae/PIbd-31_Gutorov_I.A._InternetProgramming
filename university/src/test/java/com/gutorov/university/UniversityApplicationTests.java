a7d9b49738d64e07db70b7e60bb93a495f759ae0
Ivan Gutorov <gutorov.ia1@gmail.com>
2025-09-05 01:22:30 +0400

    initialize backend Java Spring Boot project

---
+++ university/src/test/java/com/gutorov/university/UniversityApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gutorov.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
