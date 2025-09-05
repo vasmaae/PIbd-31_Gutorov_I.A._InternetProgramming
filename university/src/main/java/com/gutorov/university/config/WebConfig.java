f902b5e8b7c802d7441d34744439ab5db62e2219
Ivan Gutorov <gutorov.ia1@gmail.com>
2025-09-05 20:13:56 +0400

    add @Configuration annotation in WevConfig

--- university/src/main/java/com/gutorov/university/config/WebConfig.java
+++ university/src/main/java/com/gutorov/university/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.gutorov.university.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
