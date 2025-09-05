7c2610d9f78f489115345fbdcf28baef11c8e0f8
Ivan Gutorov <gutorov.ia1@gmail.com>
2025-09-05 20:06:45 +0400

    add swagger ui dependency

--- university/build.gradle.kts
+++ university/build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.12")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
