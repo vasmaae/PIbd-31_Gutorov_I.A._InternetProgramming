18d49531f8aa3500783fa57c0586b762b4dc95fa
Ivan Gutorov <gutorov.ia1@gmail.com>
2025-10-01 18:36:09 +0400

    add dependencies to @SpringBootApplication class, add data populating method

--- university/src/main/java/com/gutorov/university/UniversityApplication.java
+++ university/src/main/java/com/gutorov/university/UniversityApplication.java
@@ -1,11 +1,99 @@
 package com.gutorov.university;
 
+import com.gutorov.university.mapper.*;
+import com.gutorov.university.service.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Objects;
+
 @SpringBootApplication
-public class UniversityApplication {
+public class UniversityApplication implements CommandLineRunner {
+    private final Logger log = LoggerFactory.getLogger(UniversityApplication.class);
+    private final ApplicationService applicationService;
+    private final ApplicationMapper applicationMapper;
+    private final AuthorService authorService;
+    private final AuthorMapper authorMapper;
+    private final CategoryService categoryService;
+    private final CategoryMapper categoryMapper;
+    private final NewsService newsService;
+    private final NewsMapper newsMapper;
+    private final ProgramService programService;
+    private final ProgramMapper programMapper;
+
+    public UniversityApplication(ApplicationService applicationService, ApplicationMapper applicationMapper, AuthorService authorService, AuthorMapper authorMapper, CategoryService categoryService, CategoryMapper categoryMapper, NewsService newsService, NewsMapper newsMapper, ProgramService programService, ProgramMapper programMapper) {
+        this.applicationService = applicationService;
+        this.applicationMapper = applicationMapper;
+        this.authorService = authorService;
+        this.authorMapper = authorMapper;
+        this.categoryService = categoryService;
+        this.categoryMapper = categoryMapper;
+        this.newsService = newsService;
+        this.newsMapper = newsMapper;
+        this.programService = programService;
+        this.programMapper = programMapper;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(UniversityApplication.class, args);
+    }
+
+    private void populateData() {
+        log.info("Populating data");
+
+        log.info("Populating authors");
+        final var author1 = authorService.create(authorMapper.toRequest("Anton Antonov"));
+        final var author2 = authorService.create(authorMapper.toRequest("Semen Semenov"));
+        final var author3 = authorService.create(authorMapper.toRequest("Maxim Maximov"));
+
+        log.info("Population categories");
+        final var category1 = categoryService.create(categoryMapper.toRequest("Campus"));
+        final var category2 = categoryService.create(categoryMapper.toRequest("Science"));
+        final var category3 = categoryService.create(categoryMapper.toRequest("Art"));
+
+        log.info("Population news");
+        newsService.create(newsMapper.toRequest("Title 1",
+                "Content 1",
+                category1.getId(),
+                author1.getId(),
+                LocalDateTime.of(2025, Month.SEPTEMBER, 5, 17, 30, 0)
+        ));
+        newsService.create(newsMapper.toRequest("Title 2",
+                "Content 2",
+                category2.getId(),
+                author2.getId(),
+                LocalDateTime.of(2025, Month.SEPTEMBER, 5, 17, 40, 10)
+        ));
+        newsService.create(newsMapper.toRequest("Title 3",
+                "Content 3",
+                category3.getId(),
+                author3.getId(),
+                LocalDateTime.of(2025, Month.SEPTEMBER, 5, 17, 50, 20)
+        ));
+
+        log.info("Populating programs");
+        final var program1 = programService.create(programMapper.toRequest("Physics"));
+        final var program2 = programService.create(programMapper.toRequest("Maths"));
+        final var program3 = programService.create(programMapper.toRequest("Informatics"));
+
+        log.info("Populating applications");
+        applicationService.create(applicationMapper.toRequest("Ivan Ivanov", "ivanov@mail.com",
+                program1.getId(), LocalDateTime.of(2025, Month.SEPTEMBER, 5, 14, 30, 20), false));
+        applicationService.create(applicationMapper.toRequest("Petr Petrov", "petrov@mail.com",
+                program2.getId(), LocalDateTime.of(2025, Month.SEPTEMBER, 5, 14, 20, 30), true));
+        applicationService.create(applicationMapper.toRequest("Vasiliy Vasin", "vasin@mail.com",
+                program3.getId(), LocalDateTime.of(2025, Month.SEPTEMBER, 5, 14, 10, 40), false));
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (args.length == 0) return;
+
+        if (Objects.equals(args[0], "--populate")) populateData();
     }
 }
